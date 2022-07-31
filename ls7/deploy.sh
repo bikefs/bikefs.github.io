299c2f30e4e1dd4f949f8a9878b26bd82063feaf
Robin Lovelace <rob00x@gmail.com>
2022-07-31 11:22:29 +0100

    Update site

--- ls7/deploy.sh
+++ ls7/deploy.sh
@@ -3,4 +3,9 @@ ls ..
 rsync -av --exclude=".*" ../ls7 ../bikefs.github.io
 cd ../bikefs.github.io/
 git status
+ls -hal ls7
 git add -A
+git status
+git commit -am 'Update site'
+git push
+cd ../ls7
